5f2de48abaf529739fb6d97e601d3b0bbe9fb47a
Davi Kawasaki <davishinjik@gmail.com>
2017-09-07 01:03:32 -0300

    Explaining partially module.exports

--- app/infra/connectionFactory.js
+++ app/infra/connectionFactory.js
@@ -14,3 +14,9 @@ function createDBConnection() {
 module.exports = function() {
     return createDBConnection;
 }
+
+// Since Node.js module loader returns the module after preparing and loading it
+// we can use the exports literal object as the way above or other options:
+
+// exports.createDBConnection = createDBConnection();
+// require('./connectionFactory').createDBConnection();
