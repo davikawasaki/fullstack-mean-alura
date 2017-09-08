b7ab8ac058065507595c0f3863a082b46831710d
Davi Kawasaki <davishinjik@gmail.com>
2017-09-08 12:18:23 -0300

    Adding socket.io to ws protocol between client/server

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 // Setting a express proprietary module that sets and starts an express server
 var app = require('./config/express')();
+// Node.js server requests handler, using express app as its server (composition to socket.io)
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.set('io', io);
 
 // Setting port as environment or 3000
 var port = process.env.port || 3000;
 
-app.listen(port, function() {
-    console.log("Running server on port " + port);
+var server = http.listen(port, function() {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('Running server at http://%s:%s', host, port);
 });
