1ba2871f21081900a98ec39ad81a997174efe209
Davi Kawasaki <davishinjik@gmail.com>
2017-09-08 15:28:03 -0300

    Adding 404 and 500 errors verification

--- config/express.js
+++ config/express.js
@@ -33,5 +33,20 @@ module.exports = function() {
         .then('infra')
         .into(app);
 
+    // Middleware to redirect 404 not found pages
+    app.use(function(req,res,next) {
+        res.status(404).render('errors/404');
+    });
+
+    // Middleware to redirect 500 server errors
+    // Has priority because has an error argument inside the anonymous function
+    app.use(function(error,req,res,next) {
+        if(process.env.NODE_ENV == 'production') {
+            res.status(500).render('errors/500');
+            return;
+        }
+        next(error);
+    });
+
     return app;
 }
